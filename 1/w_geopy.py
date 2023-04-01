74704c3cab7501a915dcd65ba93ea161c92e3bd6
haval95 <havalrezan22@gmail.com>
2023-04-01 21:51:33 +0300

    pracing geopy with pandas

---
+++ 1/w_geopy.py
@@ -0,0 +1,19 @@
+from geopy.geocoders import ArcGIS
+import pandas as pd
+
+
+
+
+arg = ArcGIS()
+
+#u can ask for geo cordinates this way
+arg.geocode("3995 23rd st, San Francisco, Ca 94114")
+
+
+df = pd.read_csv("sample.csv")
+df["Address"]=df["Address"]+ ", "+ df["City"] + ", " + df["State"] +", "+df["Country"]
+#or u can ask for geo cordinates this way which applies tht geocode method to all the rows in address
+df["Cordinates"]= df["Address"].apply(arg.geocode)
+df["Latitude"]=df["Cordinates"].apply(lambda x: x.latitude)
+df["Longitude"]=df["Cordinates"].apply(lambda x: x.longitude)
+print(df)
