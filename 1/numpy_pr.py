73f4a046d5cb32a9cee83310d68a2da0971715ae
Haval Rezan <76211550+haval95@users.noreply.github.com>
2023-04-02 04:26:45 -0700

    playing with numpy and cv2

---
+++ 1/numpy_pr.py
@@ -0,0 +1,26 @@
+import numpy 
+import cv2
+
+
+a = numpy.arange(27)
+type(a)
+a= a.reshape(3,3,3)
+print(a[0][1][2])
+n = numpy.asarray([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]])
+
+img = cv2.imread("img.png",0)
+
+bb = numpy.asarray(newImg)
+cv2.imwrite("newImg.png",img)
+img[0:2,0:3]
+
+for i in img:
+    
+    
+for i in img.T:
+    
+
+    
+for i in img.flat:
+
+stacking = numpy.hstack((newImg,newImg))
