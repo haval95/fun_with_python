39645b6d83a828a6ffacc676f456745240b7efc0
haval95 <havalrezan22@gmail.com>
2023-04-02 21:43:43 +0300

    fixing some bugs

--- 1/numpy_pr.py
+++ 1/numpy_pr.py
@@ -2,25 +2,46 @@ import numpy
 import cv2
 
 
+''' 
+    with numpy we can create 1,2,3 dimentional arraies, 
+    and we can reshape the arrays to any dimentional way we need.
+'''
 a = numpy.arange(27)
 type(a)
-a= a.reshape(3,3,3)
-print(a[0][1][2])
-n = numpy.asarray([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]])
+a= a.reshape(3,9)
+print(a)
+n = numpy.asarray([[1,2,3,4,5],[1,2,6,7,8],[1,2,10,11,12]])
+# print(n)
+# print("new")
+# print(n[1:3,2:])
 
+
+'''
+    Reading imgs as an array, it prints the rows and columns 
+    0 : read it in gray scale, 1: colorful as BRG
+'''
+#READING
 img = cv2.imread("img.png",0)
+#print(img)
 
-bb = numpy.asarray(newImg)
+#WRITING AN IMG
 cv2.imwrite("newImg.png",img)
 img[0:2,0:3]
 
-for i in img:
+# for i in img:
+#     print(i)
     
-    
-for i in img.T:
+# for i in img.T:
     
 
     
-for i in img.flat:
+# for i in img.flat:
+#     print (i)
+stacking = numpy.vstack(([1,2,3],[4,5,6]))
+# stacking = numpy.vstack(([1,2,3],[4,5,6]))
+print(stacking)
+print()
 
-stacking = numpy.hstack((newImg,newImg))
+ar1= numpy.hsplit(stacking,3)
+# ar1= numpy.vsplit(stacking,2)
+print(ar1)
--- 1/w_geopy.py
+++ 1/w_geopy.py
@@ -14,6 +14,6 @@ df = pd.read_csv("sample.csv")
 df["Address"]=df["Address"]+ ", "+ df["City"] + ", " + df["State"] +", "+df["Country"]
 #or u can ask for geo cordinates this way which applies tht geocode method to all the rows in address
 df["Cordinates"]= df["Address"].apply(arg.geocode)
-df["Latitude"]=df["Cordinates"].apply(lambda x: x.latitude)
-df["Longitude"]=df["Cordinates"].apply(lambda x: x.longitude)
+df["Latitude"]=df["Cordinates"].apply(lambda cordinate: cordinate.latitude)
+df["Longitude"]=df["Cordinates"].apply(lambda cordinate: cordinate.longitude)
 print(df)
