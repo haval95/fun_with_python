da081c01a3be8c7419e47d5109d7b33989b1e42c
haval95 <havalrezan22@gmail.com>
2023-04-19 20:30:37 +0300

    FEAT: get graph data from a csv file using pandas

--- bukeh/basic_graphs.py
+++ bukeh/basic_graphs.py
@@ -1,17 +1,21 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
-
+import pandas as pd
 # data
 x = [1,2,3,4,5,6]
 y = [6,7,8,9,10,11]
-
 output_file("line.html")
-
 #creating a figure obj
 f = figure()
-
 # creating a line
 f.line(x,y)
+#show(f)
 
 
-show(f)
+df = pd.read_csv("data.csv")
+x = df["x"]
+y = df["y"]
+f1 = figure()
+f1.line(x,y)
+show(f1)
+
